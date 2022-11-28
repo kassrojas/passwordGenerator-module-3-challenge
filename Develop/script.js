f40269348fcf7475e7374996d2729be62e0d6c97
kassrojas <kassandrarojass@gmail.com>
2022-11-28 15:22:19 -0500

    added data set and started some if statements

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,34 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
     // data set 
-
-
-
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var special = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+    var upperLetters = letters.toUpperCase();
+    var lowerLetters = letters.toLowerCase();
+    var chooseFrom = '';
+    
+    var isUpperCase = confirm ('Would you like to use uppercase?');
+    var isLowerCase = confirm ('Would you like to use lowercase?');
+    var isNumber = confirm ('Would you like to use numbers?');
+    var isSpecial = confirm ('Would you like to use special characters?');
+    
     // conditions
 
-
+    if (isUpperCase){
+        // use uppercase letters
+        console.log ('uppercase')
+      }
+      if (isLowercase){
+        // use lowercase letters
+        console.log ('lowercase')
+      }
+      if (isNumber){
+        console.log ('number')
+      }
+      if (isSpecial){
+        console.log ('special')
+      }
 
     return '';
 }
