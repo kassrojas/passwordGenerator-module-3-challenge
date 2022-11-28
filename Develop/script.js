0942f2bfa517cb1be469edfb588a8509a6d0e31c
kassrojas <kassandrarojass@gmail.com>
2022-11-28 15:10:07 -0500

    setting up function, need to fill in blanks

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+    // data set 
+
+
+
+    // conditions
+
+
+
+    return '';
+}
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+    
+  }
+
+  // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
