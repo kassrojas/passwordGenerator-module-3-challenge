2f872e09711536398abe86e487769d1bff31e59c
kassrojas <kassandrarojass@gmail.com>
2022-11-30 15:11:41 -0500

    added some notes on pop ups, still working on if statements and for loops

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,9 @@
 //input is validated and at least one character type should be selected
 //password is generated at RANDOM
 //password is displayed in an alert
+// prompt(message) displays pop up and takes in user input
+// alert(message) pop up and OK button
+// confirm(message) pop up with OK and CANCEL button
 
 
 var generateBtn = document.querySelector("#generate");
@@ -20,33 +23,49 @@ function generatePassword(length) {
     var special = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
     var upperLetters = letters.toUpperCase();
     var lowerLetters = letters.toLowerCase();
-    var chooseFrom = '';
+    var passOption = '';
     var passwordLength = length; 
-    console.log(length);
-    var isUpperCase = confirm ('Would you like to use uppercase?');
+    // console.log(length);
+
+    // confirm will give a boolean value to use in if statement later
+    var isUpperCase = confirm ('Would you like to use uppercase?'); 
     var isLowerCase = confirm ('Would you like to use lowercase?');
     var isNumber = confirm ('Would you like to use numbers?');
     var isSpecial = confirm ('Would you like to use special characters?');
+    var randomizer = Math.floor(Math.random() * passOption.length);
     
     // conditions
 
-    if (isUpperCase){
-        chooseFrom += upperLetters;
+    if (isUpperCase){   // takes in boolean value from var isUpperCase
+        passOption += upperLetters;
       }
       if (isLowerCase){
-        chooseFrom += lowerLetters;
+        passOption += lowerLetters;
       }
       if (isNumber){
-        chooseFrom += numbers;
+        passOption += numbers;
       }
       if (isSpecial){
-        chooseFrom += special;
+        passOption += special;
       }
+
+    if (!isUpperCase && !isLowerCase && !isNumber && !isSpecial){
+        alert('')
+        return;
+    }
+
+    if (passwordLength < 8 || passwordLength > 128){
+        alert('Please enter a valid character count between 8 and 128 characters.');
+    } else {
+        // collect passwordLength and pass this value to passOptions?
+    }
        
+    for (var i = 0; i < passOption.length; i++) {
+        // run some code
+    }
       
-      var random = Math.floor(Math.random() * chooseFrom.length);
-      console.log(random);
-      console.log(chooseFrom.length);
+      
+      
       var password = '12345678';
       return password;
 }
